5faca8fc5a94bc00abe5d10dd3138885e4d661a4
Miro Renda <Miroslav.Renda@tesena.com>
2024-12-22 13:00:10 +0100

    Spike.js a scalability.js finalizovány

--- scalability.js
+++ scalability.js
@@ -4,32 +4,28 @@ import { sleep } from "k6";
 
 export let options = {
   stages: [
-    {
-      duration: "10m",
-      target: 2, // Slowly ramp up to 2 users over 10 minutes
-    },
-    {
-      duration: "10m",
-      target: 5, // Increase to 5 users over 10 minutes
-    },
-    {
-      duration: "2h40m",
-      target: 10, // Maintain 10 users for 2 hours and 40 minutes
-    },
-    {
-      duration: "10m",
-      target: 5, // Gradually decrease to 5 users over 10 minutes
-    },
-    {
-      duration: "10m",
-      target: 2, // Further decrease to 2 users over 10 minutes
-    },
-    {
-      duration: "10m",
-      target: 0, // Finally ramp down to 0 users over 10 minutes
-    },
+    { duration: "10s", target: 50, },   // ramp-up fáze pro první stabilní zátěž
+    { duration: "2m", target: 50, },    // stabilní zátěž 50 VU po dobu 2m
+    { duration: "10s", target: 100, },  // ramp-up fáze pro druhou stabilní zátěž
+    { duration: "2m", target: 100, },   // stabilní zátěž 100 VU po dobu 2m
+    { duration: "10s", target: 150, },  // ramp-up fáze pro třetí stabilní zátěž
+    { duration: "2m", target: 150, },   // stabilní zátěž 150 VU po dobu 2m
+    { duration: "10s", target: 200, },  // ramp-up fáze pro čtvrtou stabilní zátěž
+    { duration: "2m", target: 200, },   // stabilní zátěž 200 VU po dobu 2m
+    { duration: "10s", target: 250, },  // ramp-up fáze pro pátou stabilní zátěž
+    { duration: "2m", target: 250, },   // stabilní zátěž 250 VU po dobu 2m
+    { duration: "10s", target: 300, },  // ramp-up fáze pro šestou stabilní zátěž
+    { duration: "2m", target: 300, },   // stabilní zátěž 300 VU po dobu 2m
+    { duration: "10s", target: 350, },  // ramp-up fáze pro sedmou stabilní zátěž
+    { duration: "2m", target: 350, },   // stabilní zátěž 350 VU po dobu 2m
+    { duration: "10s", target: 400, },  // ramp-up fáze pro osmou stabilní zátěž
+    { duration: "2m", target: 400, },   // stabilní zátěž 400 VU po dobu 2m
+    { duration: "10s", target: 450, },  // ramp-up fáze pro devátou stabilní zátěž
+    { duration: "2m", target: 450, },   // stabilní zátěž 450 VU po dobu 2m
+    { duration: "30s", target: 0, },    // ramp-down fáze 
   ],
 };
+
 
 // vu script
 export default function () {
--- spike.js
+++ spike.js
@@ -4,34 +4,18 @@ import { sleep } from "k6";
 // init
 export let options = {
   stages: [
-    {
-      duration: "1m",
-      target: 100,
-    },
-    {
-      duration: "1m",
-      target: 0,
-    },
-    {
-      duration: "1m",
-      target: 100,
-    },
-    {
-      duration: "1m",
-      target: 0,
-    },
-    {
-      duration: "1m",
-      target: 100,
-    },
-    {
-      duration: "1m",
-      target: 0,
-    },
+    { duration: "20s", target: 120, }, // ramp-up fáze
+    { duration: "40s", target: 120, }, // stabilní zátěž 120VU
+    { duration: "25s", target: 400, }, // náběh první špičky
+    { duration: "25s", target: 120, }, // sestupná hrana první špičky
+    { duration: "70s", target: 120, }, // stabilní zátěž 120VU
+    { duration: "25s", target: 400, }, // náběh druhé špičky
+    { duration: "25s", target: 120, }, // sestupná hrana první špičky
+    { duration: "20s", target: 0, },    // ramp-down fáze 
   ],
 };
 
-// vu script
+// test script
 export default function () {
   http.get("https://test.k6.io");
   sleep(1);
